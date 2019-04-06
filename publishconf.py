ac59690ca0437921d596950d755940f189abe04a
Simon Larsén <slarse@kth.se>
2019-04-06 11:45:34 +0200

    Disable feed generation in production

--- publishconf.py
+++ publishconf.py
@@ -14,8 +14,11 @@ from pelicanconf import *
 SITEURL = 'https://repobee.org'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 DELETE_OUTPUT_DIRECTORY = True
 
