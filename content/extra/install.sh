a17c71ba5245c19db392cdc0ded81bfb5a2c87a6
Simon Larsén <slarse@slar.se>
2023-03-05 20:14:59 +0100

    Update install script

--- content/extra/install.sh
+++ content/extra/install.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-if [ -z "$REPOBEE_INSTALL_DIR" ]; then
-    REPOBEE_INSTALL_DIR="$HOME/.repobee"
-fi
+set -o errexit
+set -o nounset
+
+REPOBEE_INSTALL_DIR=${REPOBEE_INSTALL_DIR:-"$HOME/.repobee"}
+REPOBEE_INSTALL_NONINTERACTIVE=${REPOBEE_INSTALL_NONINTERACTIVE:-"false"}
 
 echo "Using install dir '$REPOBEE_INSTALL_DIR'"
 REPOBEE_BIN_DIR="$REPOBEE_INSTALL_DIR/bin"
@@ -21,7 +23,7 @@ REPOBEE_COMPLETION="$REPOBEE_INSTALL_DIR/completion"
 REPOBEE_BASH_COMPLETION="$REPOBEE_COMPLETION/bash_completion.sh"
 REGISTER_PYTHON_ARGCOMPLETE="$REPOBEE_INSTALL_DIR/env/bin/register-python-argcomplete"
 
-MIN_PYTHON_VERSION=6
+MIN_PYTHON_VERSION=8
 
 function install() {
     repobee_pip_uri=$1
@@ -35,11 +37,11 @@ function install() {
 }
 
 function check_prerequisites() {
-    # check that Python 3.6+, pip and Git are installed
+    # check that Python 3.8+, pip and Git are installed
     installed_python=$(find_python)
     if [ -z "$installed_python" ]; then
         printf "\nCannot find any compatible version of Python installed.\n"
-        echo "Please install Python 3.6 or higher and then rerun this script."
+        echo "Please install Python 3.8 or higher and then rerun this script."
         echo "See https://www.python.org/downloads/ for a Python installer."
         exit 1
     else
@@ -77,13 +79,13 @@ function install_repobee() {
     source "$REPOBEE_ENV_ACTIVATE"
     ensure_pip_installed
 
-    echo "Installing RepoBee $version"
+    echo "Installing RepoBee $repobee_pip_uri"
     REPOBEE_INSTALL_DIR="$REPOBEE_INSTALL_DIR" pip_install_quiet_failfast "$repobee_pip_uri"
     create_repobee_executable
 
-    if [ ! -f "$REPOBEE_INSTALLED_PLUGINS" ]; then
-        echo "{}" > "$REPOBEE_INSTALLED_PLUGINS"
-    fi
+    # we intentionally clobber the installed plugins file to fix installations
+    # broken by failing or missing plugins
+    echo "{}" > "$REPOBEE_INSTALLED_PLUGINS"
 
     echo "Checking PATH"
     pip_install_quiet_failfast userpath
@@ -93,7 +95,7 @@ function install_repobee() {
 
 function find_python() {
     # Find an appropriate python executable
-    for exec_suffix in "3.9" "3.8" "3.7" "3.6" "3" ""; do
+    for exec_suffix in "3.11" "3.10" "3.9" "3.8" "3" ""; do
         python_exec="python$exec_suffix"
         minor_version=$(get_minor_python3_version "$python_exec")
         if [ "$minor_version" -ge "$MIN_PYTHON_VERSION" ]; then
@@ -165,10 +167,15 @@ function create_repobee_executable() {
 }
 
 function add_to_path() {
+    if [ "$REPOBEE_INSTALL_NONINTERACTIVE" = true ]; then
+        echo "Non-interactive mode, skipping PATH modification"
+        return
+    fi
+
     printf "\n$REPOBEE_BIN_DIR is not on the PATH, so to run RepoBee you must type the full path to $REPOBEE_EXECUTABLE.\n"
     echo "We can add try to add $REPOBEE_BIN_DIR to your PATH by adding it to your profile files (e.g. .bashrc, .zshrc, config.fish, etc), and then you just need to type 'repobee' to run it."
     echo "If you know how to do this manually, then we recommend that you do so such that you get it the way you like it."
-    echo "Do you want us to add try to $REPOBEE_BIN_DIR to your PATH? [y/N]: "
+    echo "Do you want us to try to add $REPOBEE_BIN_DIR to your PATH? [y/N]: "
 
     # careful with read, its options work differently in zsh and bash
     read confirm
@@ -204,49 +211,38 @@ $($REGISTER_PYTHON_ARGCOMPLETE repobee)
 function auto_complete_msg() {
     echo "
 ### TAB COMPLETION INSTRUCTIONS ###
-RepoBee supports tab completion (aka auto completion, shell completion, etc), but you need to do just a little bit of the legwork yourself. To activate tab completion for RepoBee, do the following (depending on your shell):
 
-### bash ###
-Add the following to your ~/.bashrc:
+To enable tab completion, see https://docs.repobee.org/en/stable/install.html#tab-completion
 
-    source \"$REPOBEE_BASH_COMPLETION\"
-
-### zsh ###
-Add the following to your ~/.zshrc:
-
-    autoload -Uz compinit
-    compinit
-    autoload -Uz bashcompinit
-    bashcompinit
-    source \"$REPOBEE_BASH_COMPLETION\"
-
-IMPORTANT: You should _not_ have multiple occurences of compinit and bashcompinit in your .zshrc, they should be loaded and executed only once. If you already have them in there, just make sure to source the RepoBee bash completion script after compinit and bashcompinit have been called.
-
-### other shells ###
-Sorry, we don't support tab completion for any other shells at this time :(
-
+###################################
 "
 }
 
 function resolve_repobee_pip_uri() {
     # the optional argument can be either a version tag, or a local filepath
-    if [ $1 ]; then
-        if [ -d "$1" ]; then
-            repobee_pip_uri="$1"
+    version="${1:-}"
+    if [ "$version" ]; then
+        if [ -d "$version" ]; then
+            repobee_pip_uri="$version"
         else
             repobee_pip_uri="git+$REPOBEE_HTTPS_URL.git@$version"
         fi
     else
-        repobee_pip_uri="git+$REPOBEE_HTTPS_URL.git@$(get_latest_version)"
+        repobee_pip_uri="repobee==$(get_latest_version)"
     fi
     echo "$repobee_pip_uri"
 }
 
-install "$(resolve_repobee_pip_uri $1)"
-create_autocomplete_scripts
-auto_complete_msg
+function main() {
+    version="${1:-}"
+    install "$(resolve_repobee_pip_uri $version)"
+    create_autocomplete_scripts
+    auto_complete_msg
 
-echo "
+    echo "
 RepoBee was installed successfully. To uninstall, simply remove the directory at $REPOBEE_INSTALL_DIR
 If you are having trouble, please visit the FAQ at https://repobee.readthedocs.io/en/stable/faq.html
-"
+    "
+}
+
+main "${1:-}"
